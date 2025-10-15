9b9c3f5f8f92f316c4c548c80cf3d64f8c08fa19
Gabriel Streicher <gastreicher1@gmail.com>
2025-10-15 13:34:49 -0300

    rise

--- src/utils/cub_config.py
+++ src/utils/cub_config.py
@@ -1,5 +1,5 @@
 class Config(object):
     DATA_DIR = "/kaggle/input/cub-200-2011/images" #its expected to have 2 folders: train and test
     SAVE_MODEL_DIR = None
-    MODEL_GRADCAM = "../model/best_resnet50_cub_fullfinetune.pth" 
+    MODEL = "../model/best_resnet50_cub_fullfinetune.pth" 
     OUTPUT_DIR = "../out"
