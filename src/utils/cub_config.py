70156ddb8f072954c5901649fe68042a06e32f2a
Gabriel Streicher <gastreicher1@gmail.com>
2025-10-18 10:20:27 -0300

    organizing dir

--- src/utils/cub_config.py
+++
@@ -1,6 +0,0 @@
-class Config(object):
-    DATA_DIR = "/kaggle/input/cub-200-2011/images" #its expected to have 2 folders: train and test
-    SAVE_MODEL_DIR = None
-    MODEL = "../model/best_resnet50_cub_fullfinetune.pth" 
-    MODEL_CBAM = "../model/best_resnet50_CBAM_cub.pth"
-    OUTPUT_DIR = "../out"
